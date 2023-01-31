4991a4f36d4138eb4ab59856d119ff1988b4cf11
Jonathan Steele <jonathan@steele.pro>
2023-01-31 19:23:07 +0000

    refactor: Use a route group for top-level pages
    https://beta.nextjs.org/docs/routing/defining-routes\#route-groups

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,6 @@ import { ClerkProvider } from '@clerk/nextjs/app-beta'
 
 import './globals.css'
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </head>
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
-  )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <ClerkProvider>{children}</ClerkProvider>
 }
