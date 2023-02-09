3e21704c39ec9c6a4db33a7d121b68851d641708
Jonathan Steele <jonathan@steele.pro>
2023-02-09 20:39:46 +0000

    fix: Remove duplicate `<ClerkProvider />`

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,14 +1,10 @@
-import { ClerkProvider } from '@clerk/nextjs/app-beta'
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </head>
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>{children}</body>
+    </html>
   )
 }
