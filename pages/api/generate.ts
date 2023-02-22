db8a19f0d9746c1dee9692bfc5b00564a32b8933
Jonathan Steele <jonathan@steele.pro>
2023-02-22 20:51:27 +0000

    feat: Make prompt generation per workout type

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -8,8 +8,8 @@ const openai = new OpenAIApi(
   new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 )
 
-const generatePrompt = ({ duration, movements, type }: WorkoutInputs): string =>
-  `Create me a ${duration} minute ${type} style workout, using only the following movements:
+const generateAmrapPrompt = ({ duration, movements }: WorkoutInputs): string =>
+  `Create me a ${duration} minute AMRAP style workout, using only the following movements:
 
   ${movements.join()}
   
@@ -22,9 +22,38 @@ Desired format:
 ..
 Workout:`
 
+const generateEmomPrompt = ({ duration, movements }: WorkoutInputs): string =>
+  `Create me a EMOM style workout using only the following movements:
+
+  ${movements.join()}
+
+The workout should be approximately ${duration} minutes in length, where the calculated length must be divisible by ${
+    movements.length
+  }}. Metres and repetitions should be achievable within the 1 minute interval.
+  
+Don't include a warm-up and use kilograms. Don't include any extra instructions and use as few points as required. Return in Markdown.
+
+Desired format:
+<workout_type_acronym> • <workout_duration> minutes
+- <cardio_metres> <movement>
+- <weight_repetition> <movement> (<male_weight_kg>/<female_weight_kg> kg)
+..
+Workout:`
+
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
-    const prompt = generatePrompt(req.body as WorkoutInputs)
+    const generatePrompt = (type: WorkoutInputs['type']): string => {
+      switch (type) {
+        case 'amrap':
+          return generateAmrapPrompt(req.body as WorkoutInputs)
+        case 'emom':
+          return generateEmomPrompt(req.body as WorkoutInputs)
+        default:
+          throw new Error(`Unknown workout type: ${type}`)
+      }
+    }
+
+    const prompt = generatePrompt(req.body.type)
 
     const { data: workout } = await openai.createCompletion({
       model: 'text-davinci-003',
